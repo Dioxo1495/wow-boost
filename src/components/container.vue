0d2e4575897a3b3a6ae8cbd452a30cf90d8f2505
Willem Franco <willemfranco@gmail.com>
2023-07-16 11:36:50 -0500

    add container component

---
+++ src/components/container.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-3 bg-cover bg-gray-400" :style="{backgroundImage: src ? `url(${src})` : undefined}">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const {src} = defineProps(['src']);
+</script>
