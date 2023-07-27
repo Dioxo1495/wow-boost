0bb7d01f2a0581bda9919ccb0125235be11135e2
Willem Franco <willemfranco@gmail.com>
2023-07-27 18:15:38 -0500

    Fixing responsive - adding discord in the header

--- docs/services/index.vue
+++ docs/services/index.vue
@@ -2,7 +2,7 @@
   <main class="grid gap-3">
     <div class="md:container mx-auto grid gap-10 p-3 text-white text-center">
       <h2 class="mt-5 text-4xl text-center lg:font-semibold text-red-600 dark:text-white">Popular Services:</h2>
-      <div class="flex gap-5 p-3 justify-around">
+      <div class="flex flex-wrap gap-5 p-3 justify-around">
         <div class="flex flex-col justify-end border border-gray-400 rounded p-3 max-w-sm w-72 h-80 p-3 bg-cover hover:scale-110 transition-transform" style="background-image: url(/img/popular-cardbg-1.png);">
           <h3 class="text-2xl font-bold">Mythic+ Keys</h3>
           <ul class="list-disc list-inside text-left">
@@ -32,13 +32,13 @@
     <div class="p-5 bg-white text-black">
       <div class="md:container mx-auto flex flex-col gap-10">
         <h2 class="text-4xl text-center">EASY STEPS TO CONTACT US FAST AND EASY</h2>
-        <div class="grid grid-cols-3 gap-5">
+        <div class="flex flex-wrap justify-around gap-5">
           <div class="flex flex-col gap-5">
             <img src="/img/arrow-1.png" class="pl-8 w-28 mx-auto">
             <img src="/img/discord.svg" class="h-28 mx-auto">
             <h1 class="text-center uppercase font-semibold">Join our discord</h1>
             <div class="flex justify-center">
-              <a href="<discord>" class="inline flex gap-2 bg-[#5865f2] hover:bg-[#5865f2]/90 py-2 px-3 rounded-full text-white">
+              <a href="#" target="_blank" class="inline flex gap-2 bg-[#5865f2] hover:bg-[#5865f2]/90 py-2 px-3 rounded-full text-white">
                 JOIN NOW
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34" class="w-6">
                   <path fill="currentColor" d="M27.502 6.953A25.28 25.28 0 0 0 21.162 5c-.279.48-.589 1.131-.806 1.643a24.233 24.233 0 0 0-7.022 0A20.16 20.16 0 0 0 12.528 5a24.937 24.937 0 0 0-6.341 1.953c-4.014 5.92-5.1 11.702-4.557 17.405 2.666 1.938 5.239 3.116 7.767 3.89a18.896 18.896 0 0 0 1.658-2.68 16.264 16.264 0 0 1-2.62-1.256c.218-.155.435-.325.636-.496 5.053 2.31 10.526 2.31 15.517 0 .217.17.418.341.635.496-.837.496-1.705.915-2.62 1.255.481.946 1.04 1.845 1.66 2.682a25.694 25.694 0 0 0 7.766-3.89c.666-6.603-1.056-12.338-4.528-17.406ZM11.752 20.84c-1.518 0-2.758-1.38-2.758-3.069s1.209-3.069 2.759-3.069c1.534 0 2.79 1.38 2.758 3.07 0 1.689-1.224 3.068-2.758 3.068Zm10.185 0c-1.519 0-2.76-1.38-2.76-3.069s1.21-3.069 2.76-3.069c1.535 0 2.79 1.38 2.76 3.07 0 1.689-1.21 3.068-2.76 3.068Z"/>
@@ -56,15 +56,13 @@
             <img src="/img/support.svg" class="h-28 mx-auto">
             <h1 class="text-center uppercase font-semibold">Wait for our team <br> to contact you</h1>
           </div>
-          <div class="flex justify-center">
-        </div>
         </div>
       </div>
     </div>
-    <div class="md:container mx-auto p-5">
-      <h2 class="text-2xl text-red-600">Services</h2>
-      <div class="grid grid-cols-[1fr_auto] gap-5">
-        <ul class="list-disc list-inside">
+    <div class="md:container mx-auto p-5 grid gap-3">
+      <h2 class="text-2xl text-red-600 text-center md:text-left">Services</h2>
+      <div class="flex flex-wrap justify-center md:justify-between gap-5">
+        <ul class="list-disc list-inside border md:border-0 p-5 rounded">
           <li>Powerleveling</li>
           <li>Mythic+ keys</li>
           <li>Specific item farming in dungeons</li>
@@ -103,9 +101,11 @@
     </div>
   </main>
   <footer class="bg-white dark:bg-black border-t">
-    <div class="md:container mx-auto p-3 flex gap-5 justify-between items-center">
-      <img class="w-10" src="/img/logo-icon.png" alt="">
+    <div class="md:container mx-auto p-3 flex flex-wrap gap-5 justify-around it md:justify-between items-center">
       <div class="flex gap-5">
+        <img class="w-10" src="/img/logo-icon.png" alt="">
+      </div>
+      <div class="flex flex-wrap justify-around gap-5 mx-auto order-last lg:order-none">
         <img class="h-10" src="/img/paypal-logo.png" alt="">
         <img class="h-10" src="/img/binance-logo.png" alt="">
         <img class="h-10" src="/img/theter-logo.png" alt="">
